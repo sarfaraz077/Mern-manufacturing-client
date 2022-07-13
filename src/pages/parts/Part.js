a4fc87263c5a859be917e1d5cd9cebdce77050e3
unknown <tr9836859@gmail.com>
2022-07-13 12:32:12 +0600

    for the card

--- src/pages/parts/Part.js
+++ src/pages/parts/Part.js
@@ -1,36 +1,58 @@
 import React from "react";
+import { BsFillBagCheckFill } from "react-icons/bs";
+import Card from "react-animated-3d-card";
 
 const Part = ({ part }) => {
-  const { name, img, price, stock, MinimumOrder } = part;
+  const { name, img, price, stock, MinimumOrder, description } = part;
   return (
     <div>
-      <div class="card lg:max-w-lg header text-primary-content">
-        <div class="card-body">
-          <img
-            src={img}
-            alt=""
-            className="rounded-t-lg h-[350px] object-cover"
-          />
-          <h2 class="card-title text-2xl my-3">{name}</h2>
-          <h2 className="text-xl">
-            price :{" "}
-            <span
-              style={{
-                fontSize: "1rem",
-                fontWeight: "800",
-                letterSpacing: "0.1rem",
-                backgroundImage: "linear-gradient(to right, #ba81cf, #6886d4)",
-                "-webkit-background-clip": "text",
-                "-webkit-text-fill-color": "transparent",
-              }}
-            >
-              {price}
-            </span>
-          </h2>
-          <h2 class="text-xl my-2">Stock : {stock}</h2>
-          <h4 class="text-xl">Minimum Order : {MinimumOrder}</h4>
+      <Card
+        style={{
+          cursor: "pointer",
+          width: "350px",
+
+          backgroundImage: "linear-gradient(135deg, #536976 10%, #292E49 100%)",
+        }}
+      >
+        <div
+          class="card sm:max-w-sm lg:max-w-lg bg-clip-padding backdrop-filter  backdrop-blur-xl bg-opacity-60 border border-gray-200 text-primary-content header "
+          style={{ backdropFilter: "blur(20px)" }}
+        >
+          <div class="card-body">
+            <img
+              src={img}
+              alt=""
+              className="rounded-t-lg  object-cover  h-[250px]"
+            />
+            <h2 class="card-title text-2xl my-3">{name}</h2>
+            <h2>{description}</h2>
+
+            <h2 className="text-xl">
+              price :{" "}
+              <span
+                style={{
+                  fontSize: "1rem",
+                  fontWeight: "800",
+                  letterSpacing: "0.1rem",
+                  backgroundImage:
+                    "linear-gradient(to right, #ba81cf, #6886d4)",
+                  "-webkit-background-clip": "text",
+                  "-webkit-text-fill-color": "transparent",
+                }}
+              >
+                {price}
+              </span>
+            </h2>
+            <h2 class="text-xl my-2">Stock : {stock}</h2>
+            <h4 class="text-xl">Minimum Order : {MinimumOrder}</h4>
+
+            <button className="btn mt-2">
+              <BsFillBagCheckFill className="mx-2"></BsFillBagCheckFill>
+              Buy Now
+            </button>
+          </div>
         </div>
-      </div>
+      </Card>
     </div>
   );
 };
--- src/pages/parts/Parts.js
+++ src/pages/parts/Parts.js
@@ -9,7 +9,7 @@ const Parts = () => {
 
     data: parts,
   } = useQuery(["part", "parts"], () =>
-    fetch("http://localhost:5000/part").then((res) => res.json())
+    fetch("products.json").then((res) => res.json())
   );
   if (isLoading) {
     return <LoginLoding></LoginLoding>;
@@ -17,7 +17,8 @@ const Parts = () => {
   return (
     <>
       <h2 className="text-center text-5xl my-10 mt-20 ">Latest Tools</h2>
-      <div className="lg:px-20 grid lg:grid-cols-3 gap-7 grid-cols-1 ">
+
+      <div className=" lg:w-[100%] lg:px-20  grid lg:grid-cols-3 grid-cols-1 gap-10 ">
         {parts?.map((part) => (
           <Part key={part?._id} part={part}></Part>
         ))}
